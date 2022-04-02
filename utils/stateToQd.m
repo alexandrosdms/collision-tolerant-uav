e8d4e33bb86277e581d35bca4db6dbcd1c8b2411
Alexandros Dimas <alexandrosdms98@gmail.com>
2022-04-02 15:25:20 +0300

    fix docs

--- utils/stateToQd.m
+++ utils/stateToQd.m
@@ -1,13 +1,7 @@
 function [qd] = stateToQd(x)
-<<<<<<< HEAD
 %Converts x vector used in simulation to qd struct used in hardware
-% x is 1 x 13 vector of state variables [pos vel quat omega]
+% x is 1 x 24 vector of state variables [pos vel quat omega]
 % qd is a struct including the fields pos, vel, euler, and omega
-=======
-%Converts qd struct used in hardware to x vector used in simulation
-% x is 1 x 24 vector of state variables [pos vel omega rotm ei eI]
-% qd is a struct including the fields pos, vel, rotm, and omega
->>>>>>> change-state-representation
 
 %current state
 qd.pos = x(1:3);
