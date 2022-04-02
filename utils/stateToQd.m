5dbffc3137db2cc733cb475cc400abb428e93560
Alexandros Dimas <alexandrosdms98@gmail.com>
2022-04-02 14:16:30 +0300

    Updated the struct assigner

--- utils/stateToQd.m
+++ utils/stateToQd.m
@@ -1,16 +1,15 @@
 function [qd] = stateToQd(x)
 %Converts qd struct used in hardware to x vector used in simulation
-% x is 1 x 13 vector of state variables [pos vel quat omega]
-% qd is a struct including the fields pos, vel, euler, and omega
+% x is 1 x 24 vector of state variables [pos vel omega rotm ei eI]
+% qd is a struct including the fields pos, vel, rotm, and omega
 
 %current state
 qd.pos = x(1:3);
 qd.vel = x(4:6);
 
-Rot = QuatToRot(x(7:10)');
-[phi, theta, yaw] = RotToRPY_ZXY(Rot);
+Rot = reshape(x(10:18),3,3);
+qd.rotm = Rot;
 
-qd.rot = [phi; theta; yaw];
-qd.omega = x(11:13);
+qd.omega = x(7:9);
 
 end
